85d635b674503d5564ddf08df2ce2d7adc3d29e2
Priyanka B <86424906+14priyankab@users.noreply.github.com>
2024-09-09 18:23:31 +0530

    Update Soundex.cs

--- Soundex.cs
+++ Soundex.cs
@@ -1,68 +1,113 @@
-using System;
-using System.Text;
-
 public class Soundex
-{
-    public static string GenerateSoundex(string name)
-    {
-        if (string.IsNullOrEmpty(name))
-        {
-            return string.Empty;
-        }
 
+{
+
+   public static string GenerateSoundex(string name)
+
+    {
+
+        return EmptyStringChecker(name);
+ 
         StringBuilder soundex = new StringBuilder();
+
         soundex.Append(char.ToUpper(name[0]));
+
+        SoundexCodeAppender(name, soundex);
+ 
+        while (soundex.Length < 4)
+
+        {
+
+            soundex.Append('0');
+
+        }
+ 
+        return soundex.ToString();
+
+    }
+ 
+    private static string EmptyStringChecker(string value)
+
+    {
+
+        if (string.IsNullOrEmpty(value))
+
+            return string.Empty;
+
+    }
+ 
+    private static void SoundexCodeAppender(string name, StringBuilder soundex)
+
+    {
+
         char prevCode = GetSoundexCode(name[0]);
 
-        for (int i = 1; i < name.Length && soundex.Length < 4; i++)
+        for (int i = 0; i < IsValidNameAndSoundex(name, soundex)) ; i++)
+
         {
+
             char code = GetSoundexCode(name[i]);
-            if (code != '0' && code != prevCode)
+
+            if (IsValidSoundexCode(code, prevCode))
+
             {
+
                 soundex.Append(code);
+
                 prevCode = code;
+
             }
+
         }
 
-        while (soundex.Length < 4)
-        {
-            soundex.Append('0');
-        }
-
-        return soundex.ToString();
     }
+ 
+    private static bool IsValidSoundexCode(string code, string prevCode)
 
-    private static char GetSoundexCode(char c)
     {
-        c = char.ToUpper(c);
-        switch (c)
-        {
-            case 'B':
-            case 'F':
-            case 'P':
-            case 'V':
-                return '1';
-            case 'C':
-            case 'G':
-            case 'J':
-            case 'K':
-            case 'Q':
-            case 'S':
-            case 'X':
-            case 'Z':
-                return '2';
-            case 'D':
-            case 'T':
-                return '3';
-            case 'L':
-                return '4';
-            case 'M':
-            case 'N':
-                return '5';
-            case 'R':
-                return '6';
-            default:
-                return '0'; // For A, E, I, O, U, H, W, Y
-        }
+
+        return code != '0' && code != prevCode;
+
     }
+ 
+    private static bool IsValidNameAndSoundex(string name, StringBuilder Soundex)
+
+    {
+
+        return name.Length && Soundex.Length < 4;
+
+    }
+ 
+    private static char GetSoundexCode(char c)
+
+    {
+
+        c = char.ToUpper(c);
+
+        var soundMap = new Dictionary<char, char>
+
+        {
+
+             { 'B', '1' }, { 'F', '1' }, { 'P', '1' }, { 'V', '1' },
+ 
+             { 'C', '2' }, { 'G', '2' }, { 'J', '2' }, { 'K', '2' },
+
+             { 'Q', '2' }, { 'S', '2' }, { 'X', '2' }, { 'Z', '2' },
+ 
+             { 'D', '3' }, { 'T', '3' },
+ 
+             { 'L', '4' },
+ 
+             { 'M', '5' }, { 'N', '5' },
+ 
+             { 'R', '6' }
+
+        };
+ 
+        return soundMap.TryGetValue(c, out char codeValue) ? codeValue : '0';
+
+    }
+
 }
+check if the ccn is not more than 3
+ 
